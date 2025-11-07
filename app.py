cf35bf3f63792ff5b566907b284cdf8acff427a7
Anurag Jambhulkar <jambhulkaranurag12@gmail.com>
2025-11-07 23:41:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -403,4 +403,5 @@ def build_interface():
 
 if __name__ == "__main__":
     app = build_interface()
-    app.launch(server_name="0.0.0.0", server_port=7865, share=True)
+    port = int(os.environ.get("PORT", 7860))
+    app.launch(server_name="0.0.0.0", server_port=port)
